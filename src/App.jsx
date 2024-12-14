6365c1c19897e5eb3e7716c1dbc42331548664bf
Simon Gallien <simongallien@orange.fr>
2024-12-14 22:06:13 +0100

    Utilisation du composant Hero de DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Carousel } from "./_components/Carousel";
 import { Competences } from "./_components/Competences";
 import { Contact } from "./_components/Contact";
 import Footer from "./_components/Footer";
@@ -5,7 +6,6 @@ import Header from "./_components/Header";
 import { Hero } from "./_components/Hero";
 import { Projets } from "./_components/Projets";
 import { Spacing } from "./_components/Spacing";
-import { TimeLine } from "./_components/TimeLine";
 
 
 export default function App() {
@@ -19,10 +19,6 @@ export default function App() {
         <Projets />
         <Spacing size="md" />
         <Competences />
-        <Spacing size="md" />
-        <TimeLine />
-        <Spacing size="md" />
-        <Contact />
         <Spacing size="md" />
       </main>
       <Footer />
--- src/_components/Hero.jsx
+++ src/_components/Hero.jsx
@@ -1,30 +1,20 @@
 export const Hero = () => {
     return (
         <>
-            <div className="flex w-full">
-                <div className="flex-[2] flex flex-col gap-2 place-content-between">
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img
+                    src="/profil.jpg" 
+                    className="max-w-sm rounded-full shadow-2xl" />
                     <div>
-                        <h1 className='text-5xl font-anta font-bold mb-2.5'>Simon GALLIEN</h1>
-                        <h2 className="text-2xl font-bold mb-4">Développeur Web</h2>
-                        <p>Je suis un ingénieur spécialisé dans la course automobile et passioné par les nouvelles technologies. <br />
-                        Actuellement en formation chez OpenClassrooms dans le cursus de "Développeur Web".<br/>
-                        Je suis à la recherche d'opportunités en freelance ou en CDI pour collaborer sur des projets innovants, où je pourrais mettre à profit mes compétences techniques et ma rigueur acquise dans le domaine de l'ingénierie.
-                         </p>
+                        <h1 className="text-5xl font-bold">Simon GALLIEN</h1>
+                        <p className="py-6">
+                            Je suis un ingénieur spécialisé dans la course automobile et passioné par les nouvelles technologies. <br />
+                            Actuellement en formation chez OpenClassrooms dans le cursus de "Développeur Web".<br/>
+                            Je suis à la recherche d'opportunités en freelance ou en CDI pour collaborer sur des projets innovants, où je pourrais mettre à profit mes compétences techniques et ma rigueur acquise dans le domaine de l'ingénierie.
+                        </p>
+                        <button className="btn btn-primary">Télécharger mon CV</button>
                     </div>
-                    <div className="flex gap-4">
-                        <button className="btn btn-primary text-1xl font-bold flex-1">Télécharger mon CV</button>
-                        <a href="#contact">
-                            <button className="btn btn-secondary text-1xl font-bold flex-2">Me contacter</button>
-                        </a>
-                        
-                    </div>
-                </div>
-                <div className="flex-1 flex justify-end w-full">
-                    <img  
-                        src="/profil.jpg" 
-                        alt="monProfil" 
-                        className="w-full h-auto rounded-full max-w-xs"
-                    />
                 </div>
             </div>
         </>
