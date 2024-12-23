57ef3313aa0fa2299e0fff1a31bd5d8fd605a799
Simon Gallien <simongallien@orange.fr>
2024-12-23 09:45:48 +0100

    Correction du chemin de Home dans App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Error from "./pages/404";
 import ProjetDetail from "./pages/ProjetDetail";
