9c305f026e4bbb695249f3e5a9df7586f2228824
Simon Gallien <simongallien@orange.fr>
2024-12-30 23:17:52 +0100

    404.html modification du lien de l'image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import Error from "./pages/404";
 import ProjetDetail from "./pages/ProjetDetail";
 import ScrollToTop from "./_components/ScrollToTop";
 
@@ -15,8 +14,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projets/:id" element={<ProjetDetail />} />
-          <Route path="/404" element={<Error />} />
-          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
   
