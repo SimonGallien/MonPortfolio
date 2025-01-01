3a488513eacd128a277198bfc32627b3ebefa2cf
Simon Gallien <simongallien@orange.fr>
2025-01-01 21:37:39 +0100

    Amélioration de l'affichage du composant Hero, texte occupant moins de largeur et ajout de margin

--- src/_components/Hero.jsx
+++ src/_components/Hero.jsx
@@ -11,12 +11,12 @@ export const Hero = () => {
                         <p className="pb-6 text-justify">
                             Je suis à la recherche d&apos;opportunités en freelance ou en CDI pour collaborer sur des projets innovants, où je pourrais mettre à profit mes compétences techniques et ma rigueur acquise dans le domaine de l&apos;ingénierie.
                         </p>
-                        <a className="btn btn-primary rounded-3xl w-full" href="/CV_Simon Gallien.pdf" download>
+                        <a className="btn btn-primary rounded-3xl w-full mb-4" href="/CV_Simon Gallien.pdf" download>
                             <DownloadIcon size={16} className="text-foreground"/>
                             Télécharger mon CV
                         </a>
                     </div>
-                    <div className="aspect-w-1 aspect-h-1 w-full max-w-sm">
+                    <div className="flex-1 aspect-w-1 aspect-h-1 w-full flex justify-center">
                         <img
                             src="/profil.webp"
                             className="rounded-full shadow-2xl object-cover"
