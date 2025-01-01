098c6276d701f7cf3eb0cc7e4108490bf5b06043
Simon Gallien <simongallien@orange.fr>
2025-01-01 18:17:45 +0100

    Ajustement de la police sur la 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
         <>
             <Header />
             <img src="/images/404.webp" alt="Error 404" className="w-full h-96 object-cover"/>
-            <p className="py-8 w-full text-5xl text-current text-center flex justify-center items-center">Erreur 404 <br/> Cette page n'existe pas ou n'a pas été trouvé</p>
+            <p className="py-8 w-full text-xl md:text-5xl text-current text-center flex justify-center items-center">Erreur 404 <br/> Cette page n&apos;existe pas ou n&apos;a pas été trouvé</p>
         </>
     )
 }
